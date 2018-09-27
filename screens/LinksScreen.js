71b89d833b424fdfbe36ca8d683a9cf98613aebb
Jimin Park <parkjimin0@gmail.com>
2018-09-27 11:31:45 -0400

    got the ar section to work well just camera

--- screens/LinksScreen.js
+++ screens/LinksScreen.js
@@ -1,27 +1,60 @@
+import { AR } from 'expo';
+import ExpoTHREE, { AR as ThreeAR, THREE } from 'expo-three';
 import React from 'react';
-import { ScrollView, StyleSheet } from 'react-native';
-import { ExpoLinksView } from '@expo/samples';
+import { Text, View } from 'react-native';
 
-export default class LinksScreen extends React.Component {
-  static navigationOptions = {
-    title: 'Links',
-  };
+import { View as GraphicsView } from 'expo-graphics';
 
+export default class LinkScreen extends React.Component {
+  static url = 'screens/AR/Basic.js';
   render() {
     return (
-      <ScrollView style={styles.container}>
-        {/* Go ahead and delete ExpoLinksView and replace it with your
-           * content, we just wanted to provide you with some helpful links */}
-        <ExpoLinksView />
-      </ScrollView>
+      <View style={{ flex: 1 }}>
+        <GraphicsView
+          style={{ flex: 2 }}
+          onContextCreate={this.onContextCreate}
+          onRender={this.onRender}
+          onResize={this.onResize}
+          isArEnabled
+          isArRunningStateEnabled
+          isArCameraStateEnabled
+          arTrackingConfiguration={AR.TrackingConfigurations.World}
+        />
+        <View
+          style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}
+        >
+          <Text>😮 Works with any size viewport, try rotating your phone.</Text>
+        </View>
+      </View>
     );
   }
-}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    paddingTop: 15,
-    backgroundColor: '#fff',
-  },
-});
+  onContextCreate = props => {
+    AR.setPlaneDetection(AR.PlaneDetectionTypes.Horizontal);
+    this.commonSetup(props);
+  };
+
+  commonSetup = ({ gl, scale: pixelRatio, width, height }) => {
+    this.renderer = new ExpoTHREE.Renderer({
+      gl,
+      pixelRatio,
+      width,
+      height,
+    });
+
+    this.scene = new THREE.Scene();
+    this.scene.background = new ThreeAR.BackgroundTexture(this.renderer);
+    this.camera = new ThreeAR.Camera(width, height, 0.01, 1000);
+  };
+
+  onResize = ({ x, y, scale, width, height }) => {
+    this.camera.aspect = width / height;
+    this.camera.updateProjectionMatrix();
+    this.renderer.setPixelRatio(scale);
+    this.renderer.setSize(width, height);
+  };
+
+  onRender = () => {
+    this.renderer.render(this.scene, this.camera);
+  };
+}
